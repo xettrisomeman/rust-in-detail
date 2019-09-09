b52e432a99662b771a1ab0f3929219e3f71ae150
webizfabulous <webizmoxx@gmail.com>
2019-09-09 11:55:30 +0545

    Added string

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
 
-//using standard library function mem
-use std::mem;
+//string in rust
+//String is stored in heap same like array and list
+//so we have to use & whenerver we are doing somthing with string
 
-//array
 
 fn main(){
-    let xs: [i32; 3] = [1,2,4]; //array of interger 32bit total 3 value
-    println!("(First Element is {}) . {:?} and its length is {}",xs[0],xs,xs.len()); //print 1st value
-    println!("Size of the array is {}", mem::size_of_val(&xs));//using std library std:: 
-    //slice
-    println!("{:?} is element 1st and 2nd", &xs[0..2]); //&--> it is reference , we are referencing xs here
-    println!("Total elements in the array {:?}",&xs[0..=2]); //=2 says print the 2nd value also
 
+    let s = "String".to_owned(); //slice of string , it is not string but a str
+    let ss = String::from(s); //this is now a string
+
+    println!("{}",ss);
+    //cannot use s here because it it already used in ss
     
-}
+    // //it will cause an error
+    // println!("{}",s );
 
+
+    //string slicing
+    let slice = &ss[1..3];
+    println!("{}",slice );
+    
+
+    //string concatation
+    // let sss = String::from("Hello");
+    // let concat = sss + &ss; //we have to provide a reference to string whenever we are concating the string
+    
+    //printing concated string
+    // println!("{}",concat );
+
+    //new str
+    let mut ssss = String::from("Hello");
+    //pushing str
+    ssss.push_str("World");
+    println!("{}",ssss );
+
+    }
 
