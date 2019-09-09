4b33e1b38bc1beb90e35dd49d4d820ee042557c6
webizfabulous <webizmoxx@gmail.com>
2019-09-09 11:38:20 +0545

    added array

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 
+//using standard library function mem
+use std::mem;
 
-//tuples
+//array
+
 fn main(){
-    let t= (1,2,'l');
-    let f = ('k' , t);
-    println!("{} and {}", t.1 , t.2);
-    println!("{:?} and {}",f.1 , f.0);
+    let xs: [i32; 3] = [1,2,4]; //array of interger 32bit total 3 value
+    println!("(First Element is {}) . {:?} and its length is {}",xs[0],xs,xs.len()); //print 1st value
+    println!("Size of the array is {}", mem::size_of_val(&xs));//using std library std:: 
+    //slice
+    println!("{:?} is element 1st and 2nd", &xs[0..2]); //&--> it is reference , we are referencing xs here
+    println!("Total elements in the array {:?}",&xs[0..=2]); //=2 says print the 2nd value also
 
+    
 }
 
 
