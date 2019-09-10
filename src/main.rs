5b80c4eb3573fde71e4ef4ec1dfb51a4abd06373
webizfabulous <webizmoxx@gmail.com>
2019-09-10 14:59:09 +0545

    Added match

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,38 @@
 
-#[derive(Debug)] //this helps to use trait
 
-//struct
 
-//creating a struct object
-struct Object {
-    width: u32,
-    height: u32
-}
 
-// //creating a function which will hold obj variable of Object
-// fn area(obj: &Object) -> u32{
-//     //to access struct
-//     obj.width * obj.height
-//     //return the multiplication of obj.width and obj.height
+// fn main() {
+//     let x = 15;
+
+
+//     match x{
+//         2|4|6|8 => println!("This is a even"),
+//         1|3|5|7 => println!("This is a odd"),
+//         9..=16 => println!("This is in between 9 and 14"),
+//         _ => println!("OH MAI GHAAAH"),
+//     }
 // }
 
-impl Object {
-    //self means init to Object 
-    fn area(&self) -> u32{
-        self.height * self.width
-    }
 
-    fn show(&self){
-       println!("{} * {} is {}",self.height,self.height ,self.area() );
-    }
+// fn main() {
+//     let pair = (1,2);
 
-}
-
-//impl object for creating new object
-impl Object {
-    fn new(width: u32 , height: u32) -> Object {
-        Object{
-            width,
-            height,
-        }
-    }
-}
-
+//     match pair {
+//         (0,y)=> println!("y: {}",y ),
+//         (x,0)=> println!("x: {}",x ),
+//         _ => println!("Excuse me?")
+//     }
+// }
 
 
 fn main() {
-    //defining value here
-    let object: Object = Object{
-        width:100,
-        height:100
-    };
+    let pair = (5 ,5);
 
-    let obj = Object::new(11, 102); //creating new object 
-
-    //printing the are
-    println!("The area of the object is {}", object.area()); //passing value to the area as a reference
-    println!("The area of the object is {}", obj.area()); //passing value to the area as a reference   
-    obj.show();
-    object.show();
-
-    println!("{:#?}", obj);
-    println!("{:#?}", object);
+    match pair{
+        (x,y)if x==y => println!("X and Y are both equal"),
+        (x,y) if x > y => println!("X is greater than Y"),
+        (x , _) if x % 2 ==0 => println!("X is even"),
+        _ => println!("What is happening with you?")
+    }
 }
-
-
